c1b6ec0947dd696cf662683913c49565a02403c8
NikolaKasabov <n_kasabov@mail.bg>
2021-12-17 15:01:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bookkeeper!</h1>
+
+      <nav className={classes.nav}>
+        <Link to='/invoices'>Invoices</Link> | {" "}
+        <Link to='/expenses'>Expenses</Link>
+      </nav>
+
+      <Outlet />
     </div>
   );
 }
